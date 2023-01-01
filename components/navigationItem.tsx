10bb72e714d87aaada4778ccbc137f007619d207
Jaden Park <78674944+VsBJams@users.noreply.github.com>
2023-01-01 17:36:51 -0500

    Deleted unused packages, changed icons and description

--- components/navigationItem.tsx
+++ components/navigationItem.tsx
@@ -1,28 +1,30 @@
-import Link from 'next/link'
-import cn from 'classnames'
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import cn from "classnames";
+import { useRouter } from "next/router";
 
 export interface NavigationItemProps {
-  href: string
-  title: string
+  href: string;
+  title: string;
 }
 
 const NavigationItem = ({ href, title }: NavigationItemProps) => {
-  const router = useRouter()
-  const isActive = router.asPath === href
+  const router = useRouter();
+  const isActive = router.asPath === href;
 
   return (
     <Link href={href}>
       <a
         className={cn(
-          isActive ? 'font-semibold text-gray-300' : 'font-normal text-gray-100',
-          'tracking-normal inline-block sm:text-lg mdl:text-2xl my-8'
+          isActive
+            ? "font-semibold text-gray-300"
+            : "font-normal text-gray-100",
+          "tracking-normal inline-block sm:text-lg mdl:text-2xl my-8 hover:underline"
         )}
       >
         {title}
       </a>
     </Link>
-  )
-}
+  );
+};
 
-export { NavigationItem }
+export { NavigationItem };
--- utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const LINKEDIN_LINK = 'https://www.linkedin.com/in/jaehyeongpark'
-export const GITHUB_LINK = 'https://www.github.com/jaehyeongpark06'
-export const RESUME_LINK = './Jaehyeong_Park_Resume.pdf'
