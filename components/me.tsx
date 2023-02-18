8b338e1a9bc519f84f37fec6bc6780f96b731a08
Jaden Park <78674944+VsBJams@users.noreply.github.com>
2023-02-17 20:11:54 -0500

    About me Change

--- components/me.tsx
+++ components/me.tsx
@@ -5,16 +5,21 @@ export function AboutMe(): JSX.Element {
         About Me
       </h1>
       <p className="font-normal mt-4 mb-8 text-gray-200 sm:text-[1.05rem] 2xl:text-[1rem]">
-        I'm Jaehyeong, a high school student from Toronto, Canada 🇨🇦 building
-        software 👨‍💻 I find interesting.
+        Hi! I'm Jaehyeong, a high school student from Toronto, Canada 🇨🇦
+        building software 👨‍💻 I find interesting.
       </p>
       <p className="font-normal mt-4 mb-8 text-gray-200 sm:text-[1.05rem] 2xl:text-[1rem]">
-        Outside of studying, you can find me teaching others 🧑‍🏫, solving math
-        contest problems 📘, playing chess ♟️, or doing anything else I&apos;m
-        passionate about.
+        Currently, I'm a software engineering intern at a stealth startup,
+        developing critical new features to their beta product.
       </p>
       <p className="font-normal mt-4 mb-8 text-gray-200 sm:text-[1.05rem] 2xl:text-[1rem]">
-        Feel free to contact with me through my socials. I'd love to talk! 🤝
+        Outside of school & work, you can find me teaching others 🧑‍🏫, solving
+        math contest problems 📘, playing chess ♟️, or doing anything else
+        I&apos;m passionate about.
+      </p>
+      <p className="font-normal mt-4 mb-8 text-gray-200 sm:text-[1.05rem] 2xl:text-[1rem]">
+        That being said, I'd love to get to know you! Feel free to message me
+        about new opportunities or to have a conversation! 🤝
       </p>
     </div>
   );
